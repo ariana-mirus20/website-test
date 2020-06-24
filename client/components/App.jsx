feb5b8501d6ce3721bcd0117e6fbb14aea76700c
ariana-mirus <arianamirus@gmail.com>
2020-06-25 11:01:11 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import request from 'superagent'
+
+import Home from './Home'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+      <h1 className="title">Jsmiddy Website Test!</h1>
+      <Home />
+    </>
   )
 }
 
---
+++ client/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import request from 'superagent'
+
+const Home = () => {
+  return (
+    <>
+      <h2 className="home-title">This is for test purposes</h2>
+    </>
+  )
+}
+
+export default Home
